08c60942b4e8c7403ec8a132e5085de26a96e1eb
AlesiaTelpuk <em000323@g.bstu.by>
2023-04-28 14:38:15 +0300

    fix: corect mistake
    Подправлена работа сайта

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Нашли все кнопки с data-category
 const category = document.querySelectorAll('[data-category]');
-
+//console.log(category);
 //найти все карточки товара
 const CardTovars = document.querySelectorAll('[data-category-tov]');
 
@@ -19,15 +19,90 @@ category.forEach(function (item) {
 
     //скрыть все карточки товаров
     CardTovars.forEach(function (item) {
-      item.classList.add('hidden');
+      item.classList.add('hiddens');
     })
-
+    redrawPhoto(imagesCenter);
     // выбрать нужные карточки товара и показать их
     //найдем все селекторы с таким же id
     const CardTov = document.querySelectorAll('.' + this.dataset.category);
-    console.log(CardTov);
+    //console.log(CardTov);
     CardTov.forEach(function (elem) {
-      elem.classList.remove('hidden');
+      elem.classList.remove('hiddens');
     })
   })
 });
+
+
+//slider 
+//дети массивов слайдеров
+const imagesLeft = Array.from(document.querySelector('.slider_left').children);
+const imagesCenter = Array.from(document.querySelector('.slider_center').children);
+const imagesRight = Array.from(document.querySelector('.slider_right').children);
+//console.log(imagesCenter);
+
+//let categorytov = 'sneakers';
+//функция, которая меняет фотки при пролистывании
+function redrawPhoto(arr) {
+  //мне нужно менять блоки, а в примере меняются картинки
+  //допустим, если у дивов нет тега hidden, мы его добавляем, в противном случае удаляем
+  const array = Array.from(arr);
+  //console.log(array);
+  for (let i = 0; i < array.length; i++) {
+    if (arr[i].hidden == true) {
+      arr[i].hidden = false;
+    }
+    else arr[i].hidden = true;
+  }
+}
+//console.log(categorytov);
+//console.log(CardTovars);
+//function shiftPhoto(arrow) {
+//const w = window.innerWidth >= 768 ? 2 : 1;
+//if (window.innerWidth > 768) { const w = 6; }
+//else if (window.innerWidth <= 320) { const w = 1; }
+//else { const w = 4; }
+
+//if (arrow == 'left') {
+//slice вырезает и возвращает указанную часть массива
+//category = category.slice(w).concat(category.slice(0, w));
+//} else if (arrow == 'right') {
+// category = category.slice(-w).concat(category.slice(0, -w));
+// }
+//   category[categorytov] = category[categorytov].slice(w).concat(category[categorytov].slice(0, w));
+// } else if (arrow == 'right') {
+//   category[categorytov] = category[categorytov].slice(-w).concat(category[categorytov].slice(0, -w));
+// }
+//}
+//найти div с классом slider
+const slider = document.querySelector('.slider');
+//console.log(slider);
+
+const buttonLeft = document.querySelector('.button_slider_left');
+const buttonRight = document.querySelector('.button_slider_right');
+
+function sliderLeft() {
+  redrawPhoto(imagesRight);
+  buttonLeft.removeEventListener('click', sliderRight);
+  buttonRight.removeEventListener('click', sliderLeft);
+  slider.classList.add('move_left');
+  //shiftPhoto('left');
+};
+
+function sliderRight() {
+  redrawPhoto(imagesLeft);
+  buttonLeft.removeEventListener('click', sliderRight);
+  buttonRight.removeEventListener('click', sliderLeft);
+  slider.classList.add('move_right');
+  //shiftPhoto('right');
+};
+
+slider.addEventListener('animationend', () => {
+  redrawPhoto(imagesCenter);
+  slider.classList.remove('move_left');
+  slider.classList.remove('move_right');
+  buttonLeft.addEventListener('click', sliderRight);
+  buttonRight.addEventListener('click', sliderLeft);
+});
+
+buttonLeft.addEventListener('click', sliderRight);
+buttonRight.addEventListener('click', sliderLeft);
